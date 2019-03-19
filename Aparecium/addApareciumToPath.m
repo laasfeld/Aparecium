0fe46b60ec374d5c0f89d6d0132efeef4c2e5fba
tonisL <tonis.laasfeld@gmail.com>
2019-03-19 10:03:11 +0200

    Update to loading java table rendering components

--- Aparecium/addApareciumToPath.m
+++ Aparecium/addApareciumToPath.m
@@ -2,46 +2,25 @@ function addApareciumToPath()
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-% subdirs = {...
-%     {''},...
-%     {'bfmatlab'},...
-%     {'tinyxml'},...
-%     {'xml_toolbox'},...
-%     {''},...
-%     {''},...
-%     {''},...
-%     {''},...
-%     {''},...
-%     {''},...
-%     {''},...
-%     {''},...
-%     {''}};
-
 toolboxLocation = fileparts(mfilename('fullpath'));
 filename = mfilename('fullpath');
 thisPath = fileparts(filename);
-% for i=numel(subdirs):-1:1
-%     subdirs{i} = fullfile(thisPath, subdirs{i}{:});
-%     if ~isdir(subdirs{i})
-%         subdirs(i) = [];
-%     end
-% end
-% addpath(subdirs{:});
 addpath(genpath(toolboxLocation))
 try
     javaaddpath(strcat(thisPath,'\bfmatlab\bioformats_package.jar'), '-end')
     javaaddpath(strcat(thisPath,'\Aparecium Java\Sim plate.jar'), '-end')
     javaaddpath(strcat(thisPath,'\Aparecium Java\modified-dans-dbf.jar'), '-end')
     javaaddpath(strcat(thisPath,'\createTable\TableSorter.jar'), '-end')
-    javaaddpath(strcat(thisPath,'\createTable\ColoredFieldCellRenderer.jar'), '-end')
+    javaaddpath(strcat(thisPath,'\createTable\ColoredFieldCellRenderer.zip'), '-end')
 catch
     warndlg('Error loading java packages, some features might not work');
 end
 cd('Aparecium Options');
-settings = load('settings.mat');
-IQMToolsPath = settings.settings.IQMToolsPath;
-cd(IQMToolsPath);
+
 try
+    settings = load('settings.mat');
+    IQMToolsPath = settings.settings.IQMToolsPath;
+    cd(IQMToolsPath);
     installIQMtools();
 catch MException
     if strcmp(MException.message, 'Undefined function or variable installIQMtools')
@@ -49,8 +28,5 @@ catch MException
     end
     cd(thisPath);
 end
-cd(thisPath);
+    cd(thisPath);
 end
-
-
-
